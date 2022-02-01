4e9b850d69ec5b220bb0a4577008aac848c607e0
Dev Vyaas <devendravyas04@gmail.com>
2022-02-01 18:47:39 +0530

    new

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components'
+
+function Header() {
+  return <container>
+              Header
+        </container>;
+}
+
+export default Header;
+
+const Container = styled.div`
+  height: 70px;
+  background: #090b13;
+`
